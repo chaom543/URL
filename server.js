c6e98b0e9b9fb830f3ebc6c6b93ecdc1b1253be0
chaom543 <aomkondee007@gmail.com>
2023-05-19 03:15:10 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let ShortUrl = require('./models/shortUrl')
+let app = express()
+
+mongoose.connect('mongodb://127.0.0.1/urlShortener',{
+    useNewUrlParser: true, useUnifiedTopology:true
+})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async(request,response) =>{
+let shortUrls = await ShortUrl.find()
+ response.render('index',{shortUrls:shortUrls})
+
+})
+app.post('/shortUrls',async(request,response) =>{
+    await ShortUrl.create({full:request.body.fullUrl })
+
+response.redirect('/')
+})
+
+app.get('/:shortUrl', async (request, response) => {
+  const shortUrl = await ShortUrl.findOne({ short: request.params.shortUrl })
+  if (shortUrl == null) return response.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  response.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
